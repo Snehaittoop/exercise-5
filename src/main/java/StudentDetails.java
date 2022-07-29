78bd7d693485c42a6e799d73c6043d47d5ad9a2b
Snehaittoop <snehaittoop868@gmail.com>
2022-07-29 10:08:22 +0530

    added

---
+++ src/main/java/StudentDetails.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentDetails {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        ArrayList<String>Name=new ArrayList<>();
+        ArrayList<Integer>Rollnumber=new ArrayList<>();
+        ArrayList<Integer>Admissionnumber=new ArrayList<>();
+        ArrayList<String>College=new ArrayList<>();
+        while(true){
+            System.out.println("1 student entry");
+            System.out.println("2 search student Rollnumber");
+            System.out.println("3 student delete");
+            System.out.println("4 exist");
+            System.out.println("select an option");
+
+        }
+    }
+}
